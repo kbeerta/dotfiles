fc3fb23e53efc0708ca866f84b3ed9c9580ae788
kbeerta <koenbeerta@gmail.com>
2025-08-27 19:22:28 +0200

    add checkupdates to sway status

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -6,7 +6,8 @@ while true; do
     connection=$(nmcli -t -f GENERAL.CONNECTION dev show | head -n1 | cut -d: -f2)
     ip_address=$(nmcli -t -f IP4.ADDRESS dev show | head -n1 | cut -d: -f2 | cut -d/ -f1)
     battery=$(cat /sys/class/power_supply/BAT0/capacity)
+    upd=$(checkupdates 2>/dev/null | wc -l)
 
-    echo "${ip_address}@${connection}  / ${disk}  BAT: ${battery}%  ${time} "
+    echo "${ip_address}@${connection}  / ${disk}  UPD: ${upd}  BAT: ${battery}%  ${time} "
     sleep 60
 done
