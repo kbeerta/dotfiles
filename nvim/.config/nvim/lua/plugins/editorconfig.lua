2782ea8dd6e6b7fc97e8019ac0664053f9348fa2
kbeerta <koenbeerta@gmail.com>
2025-11-14 15:24:01 +0100

    improve editorconfig:trim_final_whitespace handling

--- nvim/.config/nvim/lua/plugins/editorconfig.lua
+++ nvim/.config/nvim/lua/plugins/editorconfig.lua
@@ -9,18 +9,23 @@ M.setup = function()
     local editorconfig = require("editorconfig")
     local group = vim.api.nvim_create_augroup("custom-editorconfig", {})
 
-    editorconfig.properties.vim_insert_final_blankline = function(bufnr, val)
-        assert(val == "true" or val == "false", "insert_final_blankline must be either \"true\" or \"false\"")
-
+    -- improved version of the code below which includes removing trailing blanklines
+    -- https://github.com/neovim/neovim/blob/d017f3c9a0b745e0c57feb8c92dcc852948f7301/runtime/lua/editorconfig.lua#L145
+    editorconfig.properties.trim_trailing_whitespace = function(bufnr, val)
+        assert(val == "true" or val == "false", "trim_trailing_whitespace must be either \"true\" or \"false\"")
         if val == "true" then
             vim.api.nvim_create_autocmd("BufWritePre", {
                 group = group,
                 buffer = bufnr,
                 callback = function()
                     local view = vim.fn.winsaveview()
-                    vim.api.nvim_command("silent! undojoin")
-                    vim.api.nvim_command("silent keepjumps keeppatterns $s/^.\\+$/&\\r/e")
+                    vim.api.nvim_command('silent! undojoin')
+                    vim.api.nvim_command('silent keepjumps keeppatterns %s/\\s\\+$//e')
                     vim.fn.winrestview(view)
+
+                    local n_lines = vim.api.nvim_buf_line_count(0)
+                    local last_nonblank = vim.fn.prevnonblank(n_lines)
+                    if last_nonblank < n_lines then vim.api.nvim_buf_set_lines(0, last_nonblank, n_lines, true, {}) end
                 end,
             })
         else
