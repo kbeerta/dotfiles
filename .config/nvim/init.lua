c89b1f0d9416bfab41c5d2dd39f77beccd08a6eb
kbeerta <koenbeerta@gmail.com>
2026-04-06 17:41:10 +0200

    feat: add nice-to-have neovim options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,20 @@
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.undofile = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.list = true
+vim.opt.listchars = { tab = "> ", trail = ".", nbsp = "_" }
+
+vim.opt.hlsearch = true
+
+vim.keymap.set({ "n" }, "<Esc>", "<CMD>nohlsearch<CR>", { desc = "Clear search highlight" })
+
 vim.cmd.colorscheme("catppuccin")
