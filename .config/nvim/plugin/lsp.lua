3fa086271271c8b08c27008e30ad01e344cf058b
kbeerta <koenbeerta@gmail.com>
2026-04-12 13:18:40 +0200

    feat: auto enable lsp's that are installed

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -4,8 +4,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-        if not client then 
-            return 
+        if not client then
+            return
         end
 
         if client:supports_method("textDocument/completion") then
@@ -15,3 +15,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
         end
     end
 })
+
+for _, config in ipairs(vim.lsp.get_configs()) do
+    local cmd = config.cmd
+    local name = config.name
+    if cmd and type(cmd) == "table" and type(cmd[1]) == "string" and vim.fn.exepath(cmd[1]) ~= "" then
+        vim.lsp.enable(name)
+    end
+end
